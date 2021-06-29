ae6a1260989eca0013313f232122e9e9d780c2db
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-06-29 15:52:48 -0400

    Add examples

--- DS&A Problems/countUniqueValues.js
+++ DS&A Problems/countUniqueValues.js
@@ -3,6 +3,11 @@
 // There can be negative numbers in the array,
 // but it will always be sorted.
 
+// Examples:
+// countUniqueValues([]) - 0
+// countUniqueValues([1,1,1,1,1,2]) - 2
+// countUniqueValues([-2,-1,-1,0,1,]) - 4
+
 let countUniqueValues = arr => {
   if (arr.length === 0) {
     return 0;
