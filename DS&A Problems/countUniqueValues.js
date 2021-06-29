0fa4cbbc1792c83a50c63a20bc2c514a278b355f
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-06-29 15:50:59 -0400

    Implement edge case

---
+++ DS&A Problems/countUniqueValues.js
@@ -0,0 +1,11 @@
+// Implement a function which accepts a sorted
+// array, and counts the unique values in it.
+// There can be negative numbers in the array,
+// but it will always be sorted.
+
+let countUniqueValues = arr => {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+}
