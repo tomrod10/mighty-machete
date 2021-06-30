5cf7d00e601084597fcad90be244964b206140a2
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-06-30 14:57:24 -0400

    Add examples

--- DS&A Problems/numTeams.js
+++ DS&A Problems/numTeams.js
@@ -12,4 +12,15 @@ A team is valid if:
 
 Return the number of teams you can form given the conditions. (soldiers can be part of multiple teams).
 
+Examples:
+Input: rating = [2,5,3,4,1]
+Output: 3
+Explanation: We can form three teams given the conditions. (2,3,4), (5,4,1), (5,3,1).
+
+Input: rating = [2,1,3]
+Output: 0
+Explanation: We can't form any team given the conditions.
+
+Input: rating = [1,2,3,4]
+Output: 4
 */
