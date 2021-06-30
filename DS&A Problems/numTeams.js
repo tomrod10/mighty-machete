af624d932cd6bc2ab5453ce15edaa37d54d437aa
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-06-30 15:05:12 -0400

    Write solution from leetcode's IDE

--- DS&A Problems/numTeams.js
+++ DS&A Problems/numTeams.js
@@ -24,3 +24,23 @@ Explanation: We can't form any team given the conditions.
 Input: rating = [1,2,3,4]
 Output: 4
 */
+
+// This solution is O(n^3) in time complexity
+var numTeams = function(rating) {
+  let count = 0;
+    for(let i = 0; i < rating.length - 2; i++){
+      for(let j = i+1; j < rating.length - 1; j++){
+        for(let k = j+1; k < rating.length; k++){
+          if((rating[i] > rating[j] && rating[j] > rating[k])){
+            count++;
+          } else if ((rating[i] < rating[j] && rating[j] < rating[k])){
+            count++;
+          }
+        }
+      }
+    }
+  return count;
+};
+
+let result = numTeams([2,5,3,4,1]); // 3
+console.log(result);
