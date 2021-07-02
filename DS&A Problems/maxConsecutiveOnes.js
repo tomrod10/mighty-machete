f78d5c93cf4e9838b3e933fa12d007e1bdc307fb
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-02 15:15:18 -0400

    Wrote a valid solution

--- DS&A Problems/maxConsecutiveOnes.js
+++ DS&A Problems/maxConsecutiveOnes.js
@@ -14,3 +14,50 @@ Input: nums = [1,0,1,1,0,1]
 Output: 2
 
 */
+
+var findMaxConsecutiveOnes = function(nums) {
+  // input - array of nums
+  // output - max amount of consecutive ones
+  // constraint/s - array contains only 0s and 1s
+  // edge case/s - empty array or null value
+
+  // create result variable
+  // create counter variable
+
+  // loop over our input array
+
+  // check for valid element (1)
+  // if valid
+    // increment our counter by 1
+  // if i === nums.length - 1
+    // check if result < counter
+      // if so, set result to counter
+  // check if curVal === (0)
+    // check if result < counter
+      // if so, set result to counter
+    // reset our counter
+  // end of loop
+  // return result
+
+  // example run
+
+  // result = 2;
+  // counter = 3;
+
+  let result = 0;
+  let counter = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    if (num === 1) {
+      counter++;
+      if (i === nums.length - 1) {
+        counter > result ? result = counter : result;
+      }
+    } else {
+      counter > result ? result = counter : result;
+      counter = 0;
+    }
+  }
+  return result;
+};
