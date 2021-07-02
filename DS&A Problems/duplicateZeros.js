eaaa2dd08c0106d3326f7f8d1eb9799b1f5ddcb2
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-02 19:58:45 -0400

    Alternate solution that runs in O(n) in time complexity

--- DS&A Problems/duplicateZeros.js
+++ DS&A Problems/duplicateZeros.js
@@ -18,6 +18,7 @@ Output: null
 Explanation: After calling your function, the input array is modified to: [1,2,3]
 */
 
+// Time Complexity: O(n^2) - not so good!
 var duplicateZeros = function(arr) {
     for (let i = 0; i < arr.length; i++) {
       if (i === arr.length - 1) { break; }
@@ -34,3 +35,34 @@ var duplicateZeros = function(arr) {
 
 let test1 = duplicateZeros([1,0,2,3,0,4,5,0]); // [1,0,0,2,3,0,0,4]
 console.log(test1);
+
+// Alternate solution
+// Time Complexity: O(n)
+let possibleDups = 0;
+let length = arr.length - 1;
+
+// Find number of zeros to be duplicated
+for (let left = 0; left <= length - possibleDups; left++) {
+  if (arr[left] === 0) {
+    if (left === length - possibleDups) {
+      arr[length] = 0;
+      length--;
+      break;
+    }
+    possibleDups++;
+  }
+}
+
+// Start backwards from the last element which would be part of new array
+let last = length - possibleDups;
+
+// Copy zero twice, and non zero once
+for (let i = last; i >= 0; i--) {
+  if (arr[i] === 0) {
+    arr[i + possibleDups] = 0;
+    possibleDups--;
+    arr[i + possibleDups] = 0;
+  } else {
+    arr[i + possibleDups] = arr[i];
+  }
+}
