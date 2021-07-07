cfe6dd5a5450ccdbf52cf02678ce16d1e7f851f2
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-07 11:03:18 -0400

    Create folder for course problems. Write prompt + examples for sumZero problem

---
+++ DS&A Problems/DS&A Course/sumZero.js
@@ -0,0 +1,19 @@
+/*
+Problem from [DS&A Course by Colt Steele]
+
+Prompt:
+Write a function called sumZero which accepts a sorted
+array of integers. The function should find the first
+pair where the sum is zero. Return an array that includes
+both values that sum to zero or undefined if a pair
+does not exist.
+
+Example 1:
+sumZero([-3,-2,-1,0,1,2,3]) // [-3,3]
+
+Exaple 2:
+sumZero([-2,0,1,3]) // undefined
+
+Example 3:
+sumZero([1,2,3]) // undefined
+*/
