e68acf5e82121a66585c2ab33407a6abb2d0d5c3
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-11 18:43:26 -0400

    prompt + examples

---
+++ DS&A Problems/easy/moveZeroes.js
@@ -0,0 +1,18 @@
+/*
+[Remove Duplicates from Sorted Array] from LeetCode's explore - Array module
+https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3157/
+
+Prompt:
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+*/
+
