bf69fda03a259f078c7a23058cd5a00ad25931f4
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-12 19:58:46 -0400

    Prompt + example

---
+++ DS&A Problems/easy/sortArrayByParity.js
@@ -0,0 +1,18 @@
+/*
+[Sort Array By Parity] from LeetCode's explore - Array module
+https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3260/
+
+Prompt:
+Given an array nums of non-negative integers, return an array consisting of all the even elements of nums, followed by all the odd elements of nums.
+
+You may return any answer array that satisfies this condition.
+
+Example 1:
+Input: nums = [3,1,2,4]
+Output: [2,4,3,1]
+The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+ */
+
+var sortArrayByParity = function(nums) {
+
+};
