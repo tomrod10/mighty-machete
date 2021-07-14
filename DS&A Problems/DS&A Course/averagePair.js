2aa9d04dba4c2374549998ba1f5d034589e1c958
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-14 17:47:04 -0400

    Used Multiple Pointers Method

--- DS&A Problems/DS&A Course/averagePair.js
+++ DS&A Problems/DS&A Course/averagePair.js
@@ -13,6 +13,8 @@ averagePair([1,2,3],  2.5); // true
 averagePair([1,3,3,5,6,7,10,12,19], 8); // true
 averagePair([-1,0,3,4,5,6], 4.1); // false
 averagePair([], 4); // false
+
+Used Multiple Pointers Method
 */
 
 console.time('abc');
