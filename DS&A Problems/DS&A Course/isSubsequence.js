c2ca5a7c7b8652cbf84eb33c1f3610e35dffa0c4
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-13 21:25:54 -0400

    Solution

--- DS&A Problems/DS&A Course/isSubsequence.js
+++ DS&A Problems/DS&A Course/isSubsequence.js
@@ -16,6 +16,14 @@ isSubsequence('abc', 'abracadabra'); // true
 isSubsequence('abc', 'acb'); // false (order matters)
 */
 
-function isSubsequence() {
-
+function isSubsequence(str1, str2) {
+  let i = 0;
+  let j = 0;
+  if (!str1) { return true; }
+  while(j < str2.length) {
+    if (str2[j] === str1[i]) { i++; }
+    if (i === str1.length) { return true; }
+    j++;
+  }
+  return false;
 }
