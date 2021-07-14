110f827a17abdf388288854df9ae10ef16c9e4e6
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-13 20:31:24 -0400

    prompt + examples

---
+++ DS&A Problems/DS&A Course/isSubsequence.js
@@ -0,0 +1,21 @@
+/*
+Problem from [DS&A Course by Colt Steele]
+
+Prompt:
+This function takes in two strings and checks whether
+the characters in the first string form a subsequence
+of the characters in the second string. In other words,
+the function should check whether the characters in the
+first string appeard somewhere in the second string,
+without their order changing.
+
+Examples:
+isSubsequence('hello', 'hello world'); // true
+isSubsequence('sing', 'sting'); // true
+isSubsequence('abc', 'abracadabra'); // true
+isSubsequence('abc', 'acb'); // false (order matters)
+*/
+
+function isSubsequence() {
+
+}
