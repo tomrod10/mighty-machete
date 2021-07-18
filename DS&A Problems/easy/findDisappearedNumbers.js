cfb0437a0128216d3bfd18cdd2118b735db0e0ad
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-18 15:52:45 -0400

    Solution + tests

--- DS&A Problems/easy/findDisappearedNumbers.js
+++ DS&A Problems/easy/findDisappearedNumbers.js
@@ -13,5 +13,25 @@ Output: [2]
 */
 
 var findDisappearedNumbers = function(nums) {
+  // input - array of ints
+  // output - array of missing ints in range
+  // constraint/s - Time O(n)
+  // edge case/s - negative ints | empty array | null
 
+  let missing = [];
+  let n = nums.length;
+
+  let uniqueNums = new Set(nums);
+  for (let i = 1; i <= n; i++) {
+    if (!uniqueNums.has(i)) {
+      missing.push(i);
+    }
+  }
+  return missing;
 };
+
+let test1 = findDisappearedNumbers([4,3,2,7,8,2,3,1]);
+let test2 = findDisappearedNumbers([1,1]);
+
+console.log(test1);
+console.log(test2);
