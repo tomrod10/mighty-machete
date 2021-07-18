2ea66676ea6cd15d6f276990a419d0b6de7fa626
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-18 15:54:03 -0400

    Deleted comments

--- DS&A Problems/easy/findDisappearedNumbers.js
+++ DS&A Problems/easy/findDisappearedNumbers.js
@@ -11,13 +11,8 @@ Example 2:
 Input: nums = [1,1]
 Output: [2]
 */
-
+console.time('a');
 var findDisappearedNumbers = function(nums) {
-  // input - array of ints
-  // output - array of missing ints in range
-  // constraint/s - Time O(n)
-  // edge case/s - negative ints | empty array | null
-
   let missing = [];
   let n = nums.length;
 
@@ -32,6 +27,7 @@ var findDisappearedNumbers = function(nums) {
 
 let test1 = findDisappearedNumbers([4,3,2,7,8,2,3,1]);
 let test2 = findDisappearedNumbers([1,1]);
+console.timeEnd('a');
 
 console.log(test1);
 console.log(test2);
