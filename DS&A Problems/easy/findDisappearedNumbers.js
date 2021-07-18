0699041bf302f28a4bf962fb5aa14cf3b3cdd793
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-18 15:58:20 -0400

    Alternate solution by hbjORbj on LeetCode

--- DS&A Problems/easy/findDisappearedNumbers.js
+++ DS&A Problems/easy/findDisappearedNumbers.js
@@ -11,6 +11,8 @@ Example 2:
 Input: nums = [1,1]
 Output: [2]
 */
+
+// Solution
 var findDisappearedNumbers = function(nums) {
   let missing = [];
   let n = nums.length;
@@ -29,3 +31,24 @@ let test2 = findDisappearedNumbers([1,1]);
 
 console.log(test1);
 console.log(test2);
+
+// Alternate Solution by hbjORbj on LeetCode
+var findDisappearedNumbers = function(nums) {
+  let res = [];
+  for (let i = 0; i < nums.length; i++) {
+      let num = Math.abs(nums[i]);
+      let idx = num-1;
+      nums[idx] = Math.abs(nums[idx]) * -1;
+  }
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > 0) res.push(i+1);
+  }
+  return res;
+  // Time Complexity: O(N)
+  // Space Complexity: O(1)
+};
+/*
+We will scan through the input array and for every number we will use its value as an index and
+negate the number at the index. For example, if we encounter 4, we will negate the number at index 3.
+The reason the index is not four is because the array is zero-indexed.
+*/
