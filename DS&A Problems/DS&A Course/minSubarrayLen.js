f08ad92e70641608e35f866751745516eef33cf5
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-17 20:05:40 -0400

    Struggled with this medium difficulty problem. Found a solution in O(1) space and O(n) time.

--- DS&A Problems/DS&A Course/minSubarrayLen.js
+++ DS&A Problems/DS&A Course/minSubarrayLen.js
@@ -20,9 +20,44 @@ minSubarrayLen([4,3,3,8,1,2,3], 11); // 2
 minSubarrayLen([1,4,16,22,5,7,8,9,10], 95); // 0
 */
 
-let minSubarrayLen = (nums, int) => {
-  // input - positive int array, positive int value
-  // output - the minimal length of a subarray whose sum is >= int
-  // constraint/s - Time O(n) | Space O(1)
-  // edge case/s - empty array | invalid inputs
+function minSubArrayLen(nums, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
+
+  while (start < nums.length) {
+    // if current window doesn't add up to the given sum then
+		// move the window to right
+    if(total < sum && end < nums.length){
+      total += nums[end];
+			end++;
+    }
+    // if current window adds up to at least the sum given then
+		// we can shrink the window
+    else if(total >= sum){
+      minLen = Math.min(minLen, end-start);
+			total -= nums[start];
+			start++;
+    }
+    // current total less than required total but we reach the end,
+    // need this or else we'll be in an infinite loop
+    else {
+      break;
+    }
+  }
+
+  return minLen === Infinity ? 0 : minLen;
 }
+debugger;
+let test1 = minSubArrayLen([1,2,5,2,8,1,5], 10) //
+let test2 = minSubArrayLen([1,2,5,2,1,5], 9) //
+let test3 = minSubArrayLen([4,2,1,6], 1) //
+let test4 = minSubArrayLen([2,2,2,2], 8) //
+let test5 = minSubArrayLen([], 4) // null
+
+console.log(test1);
+console.log(test2);
+console.log(test3);
+console.log(test4);
+console.log(test5);
