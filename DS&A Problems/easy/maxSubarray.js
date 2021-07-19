75bc0850fcf005762780bfe7f2d55c5f728ab54c
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-19 18:18:17 -0400

    Follow up

--- DS&A Problems/easy/maxSubarray.js
+++ DS&A Problems/easy/maxSubarray.js
@@ -3,7 +3,7 @@
 Prompt:
 Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
-
+Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 
 Example 1:
 Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
@@ -60,3 +60,4 @@ let test3 = maxSubArray([5,4,-1,7,8]); // 23
 console.log(test1);
 console.log(test2);
 console.log(test3);
+
