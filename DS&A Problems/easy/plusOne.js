20d0f00491acd2ca1d793505f0735dafa8b847d8
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-19 19:45:25 -0400

    Solution

--- DS&A Problems/easy/plusOne.js
+++ DS&A Problems/easy/plusOne.js
@@ -25,5 +25,14 @@ Output: [1]
 */
 
 var plusOne = function(digits) {
-
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits
+    }
+    digits[i] = 0;
+  }
+  let result = new Array(digits.length + 1).fill(0);
+  result[0] = 1;
+  return result;
 };
