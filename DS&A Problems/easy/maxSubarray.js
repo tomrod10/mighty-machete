e21172f2b98c4409be3e4803f558b2746f2a6739
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-19 18:16:47 -0400

    Attempted solution + Alternate solution using Kadane's Algo + Tests

--- DS&A Problems/easy/maxSubarray.js
+++ DS&A Problems/easy/maxSubarray.js
@@ -19,6 +19,44 @@ Input: nums = [5,4,-1,7,8]
 Output: 23
 */
 
+// My attempted solution
+// failed on input [-2, 1]
 var maxSubArray = function(nums) {
+  if (nums === null || nums.length < 1) { return 0 }
+  if (nums.length === 1) { return nums[0] }
 
+  let i = 0;
+  let sum = nums[i];
+  let max = nums.reduce((acc, curr) => (acc + curr));
+
+  for (let j = 1; j < nums.length; j++) {
+    if (sum + nums[j] > max) {
+      max = sum + nums[j];
+    } else {
+      sum -= nums[i];
+      i++;
+      sum += nums[j];
+    }
+  }
+  return max;
 };
+
+// Alternate solution using Kadane's Algorithm
+var maxSubArray = function(nums) {
+  let sum = nums[0];
+  let max = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    sum = Math.max(nums[i], sum + nums[i]);
+    max = Math.max(sum, max);
+  }
+  return max;
+};
+
+let test1 = maxSubArray([-2,1,-3,4,-1,2,1,-5,4]); // 6
+let test2 = maxSubArray([1]); // 1
+let test3 = maxSubArray([5,4,-1,7,8]); // 23
+
+console.log(test1);
+console.log(test2);
+console.log(test3);
