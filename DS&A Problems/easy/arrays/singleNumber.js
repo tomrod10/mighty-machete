73c9fc5c0899962e8f7d4f8593e38e69954ea7f3
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-20 15:08:19 -0400

    Prompt + examples

---
+++ DS&A Problems/easy/arrays/singleNumber.js
@@ -0,0 +1,25 @@
+/* [Plus One] from LeetCode
+
+Prompt:
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Each element in the array appears twice except for one element which appears only once.
+
+Example 1:
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+Input: nums = [1]
+Output: 1
+*/
+
+var singleNumber = function(nums) {
+
+};
