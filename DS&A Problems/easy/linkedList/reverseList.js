656f83ede86989cd3a15149566b4fb2fa50ceb2d
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-27 16:47:36 -0400

    Prompt + examples

---
+++ DS&A Problems/easy/linkedList/reverseList.js
@@ -0,0 +1,21 @@
+/* [Reverse Linked List] from LeetCode's explore section
+
+Prompt:
+Given the head of a singly linked list, reverse the list, and return the reversed list. Accomplish this is O(n) Time and O(1) Space
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+*/
+
+var reverseList = function(head) {
+
+};
