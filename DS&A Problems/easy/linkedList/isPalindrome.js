5d28267cf3837f07dabcd012e1dc10b55b2a2ba9
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-07-30 15:46:51 -0400

    Prompt + examples

---
+++ DS&A Problems/easy/linkedList/isPalindrome.js
@@ -0,0 +1,19 @@
+/* [Palindrome Linked List] from LeetCode's explore section
+
+Prompt:
+Given the head of a singly linked list, return true if it is a palindrome.
+
+Example 1:
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+Input: head = [1,2]
+Output: false
+
+Follow up: Could you do it in O(n) time and O(1) space?
+ */
+
+var isPalindrome = function(head) {
+
+};
