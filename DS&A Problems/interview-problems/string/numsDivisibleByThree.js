cd67505ef0be1f94161eac9af25c49c7b74ab1b3
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-08-01 22:55:32 -0400

    Prompt + example

---
+++ DS&A Problems/interview-problems/string/numsDivisibleByThree.js
@@ -0,0 +1,30 @@
+/* [Numbers Divisible by 3]
+Prompt:
+You are given a string (S), consisting of N digits, that represents a number.
+You can change at most one digit in the string to any other. How many
+different numbers divisible by 3 can be obtained in this way?
+
+Examples:
+
+1. Given string "23", the function should return 7. All
+numbers divisible by 3 that can be obtained after at most
+one change are: "03", "21", "24", "27", "33", "63", and "93".
+
+2. Given string "0081", the function should return 11. All
+numbers divisible by 3 that can be obtained with at most one
+change are: "0021", "0051", "0081", "0084", "0087", "0381",
+"0681", "0981", "3081", "6081", and "9081".
+
+3. Given string "022", the function should return 9. All
+numbers divisible by 3 that can be obtained with at most
+one change are: "012", "021", "024", "027", "042", "072",
+"222", "522", and "822".
+
+Write an efficient algorithm for the following assumptions:
+  - N is an integer within the range [1..100,000]
+  - string S consists only of digits (0-9)
+*/
+
+var numsDivisibleByThree = function(S) {
+
+}
