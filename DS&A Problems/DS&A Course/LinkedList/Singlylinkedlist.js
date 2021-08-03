3a358f3ca134755de350e024509ba559f87ad4af
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-08-03 19:55:28 -0400

    Design Singly Linked List with methods

---
+++ DS&A Problems/DS&A Course/LinkedList/Singlylinkedlist.js
@@ -0,0 +1,64 @@
+// Designing a linked list and its methods
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  // Add to the end of the list
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  // Delete at the end of the list
+  pop() {
+    let current = this.head;
+    let newTail = this.head;
+
+    if (!this.head) return undefined;
+
+    while(current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.lenght--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  // Delete the first element of the list
+  shift() {
+    if (!head) return undefined;
+
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+}
