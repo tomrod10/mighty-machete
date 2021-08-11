698fa61793db115d19d6a2d4919bc3261f4a10ab
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-08-11 16:10:15 -0400

    Iterative solution

---
+++ DS&A Problems/easy/linkedList/extra-problems/rotateRight.js
@@ -0,0 +1,35 @@
+/* [Rotate List] from LeetCode's explore section
+
+Prompt:
+Given the head of a linked list, rotate the list to the right by k places.
+
+Example 1:
+
+[IMAGE/DIAGRAM EXPLANATION]
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+
+Example 2:
+
+[IMAGE/DIAGRAM EXPLANATION]
+
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+ var rotateRight = function(head, k) {
+
+};
