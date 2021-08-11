6e8ae606a3036c9a107774104eaea60ec52af74a
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-08-11 19:14:59 -0400

    Similar solution

--- DS&A Problems/easy/linkedList/extra-problems/addTwoNumbers.js
+++ DS&A Problems/easy/linkedList/extra-problems/addTwoNumbers.js
@@ -59,3 +59,23 @@ Output: [8,9,9,9,0,0,0,1]
   return head.next;
 };
 
+// Similar Solution
+var addTwoNumbers = function(l1, l2) {
+  let dummy = new ListNode(-1, null);
+  let head = dummy;
+  let carry = 0;
+
+  while(l1 || l2) {
+    let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+    carry = Math.floor(sum / 10);
+    dummy.next = new ListNode(sum % 10);
+    dummy = dummy.next;
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+  }
+
+  if (carry > 0) {
+    dummy.next = new ListNode(carry);
+  }
+ return head.next;
+};
