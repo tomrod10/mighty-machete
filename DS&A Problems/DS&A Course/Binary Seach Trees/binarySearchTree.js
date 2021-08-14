eb0eb34ce3436c444695f63cf13682ecd58a50fb
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-08-14 16:16:34 -0400

    Design BST class with insert method

---
+++ DS&A Problems/DS&A Course/Binary Seach Trees/binarySearchTree.js
@@ -0,0 +1,66 @@
+// Design a BST with methods
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    let newNode = new Node(val);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this.root;
+    }
+    if (val === node.val) return undefined;
+
+    let node = this.root;
+    while(true) {
+      if (val > node.val) {
+
+        if (!node.right) {
+          node.right = newNode;
+          return this;
+        } else {
+          node = node.right
+        }
+      } else if (val < node.val) {
+        if (!node.left) {
+          node.left = newNode;
+          return this;
+        }
+        else {
+          node = node.left;
+        }
+      }
+    }
+  }
+}
+
+/*
+Example tree
+
+        10
+    5      13
+  2  7   11  15
+
+*/
+
+const tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(15);
+tree.insert(7);
+
+
