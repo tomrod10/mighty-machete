7c59e8ef693f78b8a37beb15cb0674a8de9f7689
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-08-17 14:07:51 -0400

    Similar solution

--- DS&A Problems/DS&A Course/Recursion/swapPairs.js
+++ DS&A Problems/DS&A Course/Recursion/swapPairs.js
@@ -26,3 +26,21 @@ var swapPairs = function(head) {
   head.next.next = swapPairs(nextNode);
   return head;
 };
+
+// Similar Solution by JohnnyXian
+var swapPairs = function(head) {
+  if (head == null || head.next == null) return head
+
+  let currHead = head
+  let nextHead = head.next
+  let skipHead = head.next.next
+
+  // relink the second node to the first node
+  nextHead.next = currHead
+
+  // the firstNode's next would be the result of the swap
+  currHead.next = swapPairs(skipHead)
+
+  // nextHead targets the 2nd node, which would be the first node if it reversed
+  return nextHead
+};
